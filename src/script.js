889180abfa952c7fcb8b7457df64426c45b0cd07
Chris <csleary@gmail.com>
2019-06-20 13:31:43 +0200

    Contact form update, scss clean up, minor header tweaks, lazyload images.

--- src/script.js
+++ src/script.js
@@ -1,24 +1,22 @@
 $(document).ready(function() {
   setInterval(function() {
-    $("#pageTitle").shineText();
+    $('#pageTitle').shineText();
   }, 10000);
 
-  $(".discog-art").slick({
+  $('.discog-art').slick({
     arrows: false,
     autoplay: true,
     autoplaySpeed: 3000,
     fade: true,
-    cssEase: "linear",
-    lazyLoad: "ondemand"
+    cssEase: 'linear',
+    lazyLoad: 'ondemand'
   });
-});
 
-(function($, window, undefined) {
   $.fn.shineText = function(options) {
     var settings = $.extend(
       {
         speed: 50,
-        shineClass: "shine",
+        shineClass: 'shine',
         complete: null
       },
       options
@@ -27,7 +25,7 @@ $(document).ready(function() {
     return this.each(function() {
       var text = $(this).text();
       var doAnimate = function(el) {
-        el.find("span").each(function() {
+        el.find('span').each(function() {
           var that = $(this);
           setTimeout(function() {
             that.toggleClass(settings.shineClass);
@@ -36,14 +34,14 @@ $(document).ready(function() {
         });
       };
 
-      if (!$(this).hasClass("shineApplied")) {
+      if (!$(this).hasClass('shineApplied')) {
         $(this)
-          .addClass("shineApplied")
-          .html("");
+          .addClass('shineApplied')
+          .html('');
         for (i = 0; i < text.length; i++) {
-          $(this).append("<span>" + text[i] + "</span>");
+          $(this).append('<span>' + text[i] + '</span>');
         }
-        $(this).append("<span></span>");
+        $(this).append('<span></span>');
       }
       doAnimate($(this));
 
@@ -52,59 +50,54 @@ $(document).ready(function() {
       }
     });
   };
-})(jQuery, window);
+});
 
-var $contactForm = $("#contact-form");
+var $contactForm = $('#contact-form');
 
 $contactForm.submit(function(e) {
   e.preventDefault();
-  var $submit = $("input:submit", $contactForm);
+  var $submit = $('input:submit', $contactForm);
   var defaultSubmitText = $submit.val();
 
-  $.ajax({
-    url: "//formspree.io/" + "mail" + "@" + "melograf" + "." + "com",
-    method: "POST",
+  $.post({
+    url: 'https://melograf.com/api/contact',
     data: $(this).serialize(),
-    dataType: "json",
+    dataType: 'json',
     beforeSend: function() {
-      $submit.attr("disabled", true).val("Sending message…");
+      $submit.prop('disabled', true).val('Sending message…');
     },
-    success: function(data) {
-      $contactForm.append(
-        "<div class='success'>Message sent successfully! Thanks for getting in touch&mdash;I'll reply to you soon.</div>"
-      );
-      $submit.val("Message sent!");
-      $("#contact-form")[0].reset();
+    success: function(res) {
+      $contactForm.append("<div class='success'>" + res.success + '</div>');
+      $submit.val('Message sent!');
+      $('#contact-form')[0].reset();
       setTimeout(function() {
-        $(".success").fadeOut(function() {
+        $('.success').fadeOut(function() {
           $(this).remove();
         });
-        $submit.attr("disabled", false).val(defaultSubmitText);
+        $submit.prop('disabled', false).val(defaultSubmitText);
       }, 5000);
     },
     error: function(err) {
-      $contactForm.append(
-        "<div class='error'>Oh dear! We encountered an error while sending your message. Message not sent.</div>"
-      );
-      $submit.val("Sending failed, sorry.");
+      $contactForm.append("<div class='error'>" + err.error + '</div>');
+      $submit.val('Sending failed!');
       setTimeout(function() {
-        $(".error").fadeOut(function() {
+        $('.error').fadeOut(function() {
           $(this).remove();
         });
-        $submit.attr("disabled", false).val(defaultSubmitText);
+        $submit.prop('disabled', false).val(defaultSubmitText);
       }, 5000);
     }
   });
 });
 
 function quote() {
-  var extras = document.getElementById("extras").value;
-  var numTracks = document.getElementById("numTracks").value;
+  var extras = document.getElementById('extras').value;
+  var numTracks = document.getElementById('numTracks').value;
   var price;
-  var ddp = document.getElementById("ddp").checked;
-  var vat = document.getElementById("vat").checked;
+  var ddp = document.getElementById('ddp').checked;
+  var vat = document.getElementById('vat').checked;
 
-  // Work out per track price.
+  // Work out per-track price
   if (numTracks == 1) {
     price = 45;
   } else if (numTracks > 1 && numTracks <= 4) {
@@ -115,38 +108,36 @@ function quote() {
     price = 0;
   }
 
-  // Check if client has added extras without first adding track for mastering.
-  if (numTracks === "0") {
-    if (extras !== "0" && ddp === false) {
-      document.getElementById("results").innerHTML =
-        "Please add at least one track before adding extras.";
-      $("#results")
+  // Validation
+  if (numTracks === '0') {
+    if (extras !== '0' && ddp === false) {
+      document.getElementById('results').innerHTML =
+        'Please add at least one track before adding extras.';
+      $('#results')
         .stop(true, true)
-        .css("color", "red");
-    } else if (extras === "0" && ddp === true) {
-      document.getElementById("results").innerHTML =
-        "Please add at least one track before adding a DDPi/CDR.";
-      $("#results")
+        .css('color', 'red');
+    } else if (extras === '0' && ddp === true) {
+      document.getElementById('results').innerHTML =
+        'Please add at least one track before adding a DDPi/CDR.';
+      $('#results')
         .stop(true, true)
-        .css("color", "red");
+        .css('color', 'red');
     } else {
-      document.getElementById("results").innerHTML =
-        "Please add at least one track.";
-      $("#results")
+      document.getElementById('results').innerHTML = 'Please add at least one track.';
+      $('#results')
         .stop(true, true)
-        .css("color", "red");
+        .css('color', 'red');
     }
   } else {
     var subtotal = price + extras * 10 + ddp * 50;
     var vatCost = vat * subtotal * 0.21;
     var total = subtotal + vatCost;
 
-    document.getElementById("results").innerHTML =
-      "Total cost will be: &euro;" + total.toFixed(2);
-    $("#results")
+    document.getElementById('results').innerHTML = 'Total cost will be: &euro;' + total.toFixed(2);
+    $('#results')
       .stop(true, true)
-      .effect("highlight", { color: "#00ffff" })
-      .css("color", "#00ffff")
-      .animate({ color: "#fff" }, { queue: false, duration: 500 });
+      .effect('highlight', { color: '#00ffff' })
+      .css('color', '#00ffff')
+      .animate({ color: '#fff' }, { queue: false, duration: 500 });
   }
 }
